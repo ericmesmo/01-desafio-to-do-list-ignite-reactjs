117f80e3694d31b887085da6b71eec7045c05121
ericmesmo <oliveiraericm@gmail.com>
2023-02-13 11:48:34 -0300

    config vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/01-desafio-to-do-list-ignite-reactjs/',
   plugins: [react()],
 })
